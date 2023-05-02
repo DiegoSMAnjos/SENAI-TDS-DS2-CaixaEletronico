998fe4d2d2b13dc5179c95ca4bb963de9381624e
Diego Anjos <33529467+DiegoSMAnjos@users.noreply.github.com>
2023-05-01 23:25:15 -0300

    update

---
+++ model/banco.sql
@@ -0,0 +1,13 @@
+create database db_voluntario;
+use db_voluntario;
+create table tb_voluntario2(
+	id int not null auto_increment primary key,
+    nome varchar(30),
+    idade int,
+    genero varchar(100),
+    turno varchar(10)
+);
+
+describe tb_voluntario2;
+select * from tb_voluntario2;
+insert into tb_voluntario2 (nome, idade) values ("Maria", 30);
